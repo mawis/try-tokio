e0d003cfefb5a9bed64bdba3db1734cb4bcc8bf2
Matthias Wimmer <m@tthias.eu>
2017-11-28 17:29:54 +0100

    Replaced echo-server with different implementation.
    Code from: https://tokio.rs/docs/getting-started/simple-server/

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,86 @@
-// a tiny async echo server with 東京
-
+extern crate bytes;
 extern crate futures;
 extern crate tokio_core;
 extern crate tokio_io;
+extern crate tokio_proto;
+extern crate tokio_service;
 
-use futures::{Future, Stream};
-use tokio_core::net::TcpListener;
-use tokio_core::reactor::Core;
-use tokio_io::{AsyncRead, io};
+use std::io;
+use std::str;
+use bytes::BytesMut;
+use futures::{future, Future};
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::{Decoder, Encoder, Framed};
+use tokio_proto::TcpServer;
+use tokio_proto::pipeline::ServerProto;
+use tokio_service::Service;
+
+pub struct LineCodec;
+pub struct LineProto;
+pub struct Echo;
+
+impl Decoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<String>> {
+        if let Some(i) = buf.iter().position(|&b| b == b'\n') {
+            // the line
+            let line = buf.split_to(i);
+
+            // skip newline char
+            buf.split_to(1);
+
+            // line as string
+            match str::from_utf8(&line) {
+                Ok(s) => Ok(Some(s.to_string())),
+                Err(_) =>
+                    Err(io::Error::new(io::ErrorKind::Other, "invalid UTF-8"))
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl Encoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn encode(&mut self, msg: String, buf: &mut BytesMut) -> io::Result<()> {
+        buf.extend(msg.as_bytes());
+        buf.extend(b"\n");
+        Ok(())
+    }
+}
+
+impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for LineProto {
+    type Request = String;
+    type Response = String;
+
+    type Transport = Framed<T, LineCodec>;
+    type BindTransport = Result<Self::Transport, io::Error>;
+
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(LineCodec))
+    }
+}
+
+impl Service for Echo {
+    type Request = String;
+    type Response = String;
+
+    type Error = io::Error;
+
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        Box::new(future::ok(req))
+    }
+}
 
 fn main() {
-    // creating event loop
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-
-    // binding socket
     let addr = "[::1]:12345".parse().unwrap();
-    let tcp = TcpListener::bind(&addr, &handle).unwrap();
-
-    // iterating accepted connections
-    let server = tcp.incoming().for_each(|(tcp, _)| {
-        let (reader, writer) = tcp.split();
-
-        let bytes_copied = io::copy(reader, writer);
-
-        let handle_conn = bytes_copied
-            .map(|(n, _, _)| println!("wrote {} bytes", n))
-            .map_err(|err| println!("IO error {:?}", err));
-
-        handle.spawn(handle_conn);
-
-        Ok(())
-    });
-
-    core.run(server).unwrap();
+    let server = TcpServer::new(LineProto, addr);
+    server.serve(|| Ok(Echo));
 }
